7c2d480bba94eafc613c016154ca6c1fe416a7b8
Klaas Rikhotso <klaas@blackboardbs.com>
2024-03-28 12:47:33 +0200

    - Fix signin 404 error

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use \App\Models\Domain;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
@@ -115,7 +114,7 @@ class AuthController extends Controller
                 ])->setStatusCode(SymfonyResponse::HTTP_NOT_FOUND);
             }
 
-            $request = Request::create('/api/'.$domain->tenant_id.'/auth/login', 'POST');
+            $request = Request::create(env('APP_URL').'/api/'.$domain->tenant_id.'/auth/login', 'POST');
             $response = Route::dispatch($request);
 
         } catch (Throwable $exception) {
