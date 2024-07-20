7cca4a155724c30ac8f6bdc37e1a834b8d3ff464
Klaas Rikhotso <klaas@blackboardbs.com>
2024-07-20 13:10:55 +0200

    API - Add profile avatar

--- database/migrations/tenant/2024_04_14_160517_create_profiles_table.php
+++ database/migrations/tenant/2024_04_14_160517_create_profiles_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('avatar')->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('company_name')->nullable();
