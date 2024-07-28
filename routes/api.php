daef526ed924f225d6ed4922f3ed5cc26b22ee2e
Klaas Rikhotso <klaas@blackboardbs.com>
2024-07-28 14:04:42 +0200

    API - Add assign process to profile

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ Route::group([
     Route::post('/profile/create', [ProfileController::class, 'create'])->name('api.profile.create');
     Route::put('/profile/update/{id}', [ProfileController::class, 'update'])->name('api.profile.update');
     Route::delete('/profile/delete/{id?}', [ProfileController::class, 'delete'])->name('api.profile.delete');
+    Route::post('/profile/assign-process', [ProfileController::class, 'assignProcess'])->name('api.profile.assign-process');
 
     // Profile Type routes
     Route::get('/profile-type/{id?}', [ProfileTypeController::class, 'get'])->name('api.profile-type');
