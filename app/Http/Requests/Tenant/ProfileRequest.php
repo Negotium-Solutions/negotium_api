462e4ac6e8d48e67c8d209bf6003975b78656d9e
Klaas <kmrikhotso@gmail.com>
2024-10-09 04:28:20 +0200

    API - Profile creator

---
+++ app/Http/Requests/Tenant/ProfileRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests\Tenant;
+
+use App\Rules\SouthAfricanIdNumber;
+use App\Rules\SouthAfricanPhoneNumber;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $shouldValidate = false;
+        if ( $this->has('validate') && (int)$this->input('validate') === 1) {
+            $shouldValidate = true;
+        }
+
+        if($shouldValidate === false) {
+            return [];
+        }
+
+        $validationArray = [];
+        $steps = $this->input('steps');
+        foreach ($steps as $step)
+        {
+            foreach ($step['fields'] as $field) {
+                $validations = [];
+                if (!isset($field['validations'])) {
+                    continue;
+                }
+                foreach ($field['validations'] as $validation) {
+                    switch ($validation['name']) {
+                        case 'sa_id_number':
+                            $validations[] = new SouthAfricanIdNumber;
+                            break;
+                        case 'sa_phone_number':
+                            $validations[] = new SouthAfricanPhoneNumber;
+                            break;
+                        default:
+                            $validations[] = $validation['name'];
+                            break;
+                    }
+                }
+                // Build the validation rules
+                $validationArray[$field['field']] = $validations;
+                // Reconstruct the request
+                $this->merge([$field['field'] => $field['value']]);
+            }
+        }
+
+        return $validationArray;
+    }
+}
